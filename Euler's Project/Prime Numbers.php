3239b6641699f9b7f272681b23df514c1194c168
SAM <samlfstr@gmail.com>
2020-03-30 19:07:04 +0300

    Create Prime Numbers.php

---
+++ Euler's Project/Prime Numbers.php
@@ -0,0 +1,16 @@
+// Pour une quelconque nombre
+
+$testing_number = 0;
+
+for($i = 2; $i <= $testing_number; $i){
+    // Testing number kendisine kadar tüm sayılara bölünüyor
+    // Eğer tam bölünüyorsa prime değil
+    // Eğer sadece kensine bölünüyorsa prime
+    if($testing_number % $i == 0){
+        break;
+    }
+    if(i == j){
+        echo i;
+    }
+}
+
