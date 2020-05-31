728c0ef25c43f35f22f8bdc80c4e6676ff104964
SAM <samlfstr@gmail.com>
2020-05-31 03:09:12 +0300

    Create PrimeDigits.php

---
+++ Functions/PrimeDigits.php
@@ -0,0 +1,29 @@
+$primelist = array();
+fileToarray('primes', $primelist);
+
+function digits_r($prime)
+{
+    $sum = 0;
+    // take list of digits of the prime
+    $list = digits($prime);
+    for ($i = 0; $i < count($list); $i++) {
+        $sum += $list[$i];
+    }
+    // iterate if sum > 9
+    if ($sum > 9){
+        do{
+            $list = digits($sum);
+            $sum = 0;
+            for($i=0; $i < count($list); $i++){
+                $sum += $list[$i];
+            }
+        }while($sum > 9);
+    }
+    return $sum;
+}
+
+$primelist = array_map('intval',$primelist);
+
+for($i=0; $i < count($primelist); $i++){
+    echo digits_r($primelist[$i]) . br;
+}
